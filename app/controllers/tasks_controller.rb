ae699f911de8ffc96fc8d01de40573f8c1cc487a
hidetoshi.sakamoto <hidetoshisakamoto7@gmail.com>
2021-02-08 08:12:44 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   end
   
   def create 
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Taskが正常に追加されました'
